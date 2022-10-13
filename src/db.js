f98dc13f8e4ac2515ef3e67fe223bc9d3d06fa81
koechcoding <koechkelvin2001@gmail.com>
2022-10-13 11:07:20 +0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,31 @@
+var Sequelize = require("sequelize");
+var sequelize = require("./sqlitesetup");
+
+const Location = sequelize.define(
+    "location",
+    {
+        //attributes
+        name:{
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        },
+        totalfemale:{
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        totalmale: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        total: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        //options
+    }
+);
+
+exports = module.exports = location;
