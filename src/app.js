e6470d1fd5752a6addf534e04c2d0dadfc940d68
koechcoding <koechkelvin2001@gmail.com>
2022-10-13 11:36:51 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -3,3 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 var location = require("./db");
 
+const port = 8000;
+const app = express();
+
+//set up body parsers
+app.use(bodyParser.json());
+
+//Extend url parser urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
